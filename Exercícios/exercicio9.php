74b027f71364f6f07e4fef1a5d3ffc4aafe49d64
Gabrielly Zgoda <142603509+gabriellyzgoda@users.noreply.github.com>
2024-02-27 08:28:56 -0300

    Atualização 27/02

--- Exercícios/exercicio9.php
+++ Exercícios/exercicio9.php
@@ -51,16 +51,16 @@
         <input type='number' name='quinze'>
         <br>
         Digite o 16º número:
-        <input type='number' name='desesseis'>
+        <input type='number' name='dezesseis'>
         <br>
         Digite o 17º número:
-        <input type='number' name='desessete'>
+        <input type='number' name='dezessete'>
         <br>
         Digite o 18º número:
-        <input type='number' name='desoito'>
+        <input type='number' name='dezoito'>
         <br>
         Digite o 19º número:
-        <input type='number' name='desenove'>
+        <input type='number' name='dezenove'>
         <br>
         Digite o 20º número:
         <input type='number' name='vinte'>
@@ -68,14 +68,131 @@
         <input type='submit' value='Calcular'>
     </form>
     <?php
-        if(isset($_POST['um']) and isset($_POST['dois']) and isset($_POST['tres']) and isset($_POST['quatro']) and isset($_POST['cinco']) and isset($_POST['seis']) and isset($_POST['sete']) and isset($_POST['oito']) and isset($_POST['nove']) and isset($_POST['dez']) and isset($_POST['onze']) and isset($_POST['doze']) and isset($_POST['treze']) and isset($_POST['quatorze'])){
-        $livro = $_POST['livro'];
-        $usuario = $_POST['usuario'];
-        if($usuario == "P"){
-            echo "Você tem 10 dias para devolver o livro: " . $livro;
-        }if($usuario == "A"){
-            echo "Você tem 3 dias para devolver o livro: " . $livro;
-    }
+        if(isset($_POST['um']) and isset($_POST['dois']) and isset($_POST['tres']) and isset($_POST['quatro']) and isset($_POST['cinco']) and isset($_POST['seis']) and isset($_POST['sete']) and isset($_POST['oito']) and isset($_POST['nove']) and isset($_POST['dez']) and isset($_POST['onze']) and isset($_POST['doze']) and isset($_POST['treze']) and isset($_POST['quatorze']) and isset($_POST['quinze']) and isset($_POST['dezesseis']) and isset($_POST['dezessete']) and isset($_POST['dezoito']) and isset($_POST['dezenove']) and isset($_POST['vinte'])){
+            
+            $totalPositivo = 0;
+            $totalNegativo = 0;
+
+            if($_POST['um'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['um'];
+            }
+            if ($_POST['um'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['dois'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['dois'];
+            }
+            if ($_POST['dois'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['tres'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['tres'];
+            }
+            if ($_POST['tres'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['quatro'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['quatro'];
+            }
+            if ($_POST['quatro'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['cinco'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['cinco'];
+            }
+            if ($_POST['cinco'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['seis'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['seis'];
+            }
+            if ($_POST['seis'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['sete'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['sete'];
+            }
+            if ($_POST['sete'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['oito'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['oito'];
+            }
+            if ($_POST['oito'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['nove'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['nove'];
+            }
+            if ($_POST['nove'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['dez'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['dez'];
+            }
+            if ($_POST['dez'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['onze'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['onze'];
+            }
+            if ($_POST['onze'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['doze'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['doze'];
+            }
+            if ($_POST['doze'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['treze'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['treze'];
+            }
+            if ($_POST['treze'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['quatorze'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['quatorze'];
+            }
+            if ($_POST['quatorze'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['quinze'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['quinze'];
+            }
+            if ($_POST['quinze'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['dezesseis'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['dezesseis'];
+            }
+            if ($_POST['dezesseis'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['dezessete'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['dezessete'];
+            }
+            if ($_POST['dezessete'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['dezoito'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['dezoito'];
+            }
+            if ($_POST['dezoito'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['dezenove'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['dezenove'];
+            }
+            if ($_POST['dezenove'] < 0){
+                $totalNegativo++;
+            }
+            if($_POST['vinte'] > 0){
+                $totalPositivo = $totalPositivo + $_POST['vinte'];
+            }
+            if ($_POST['vinte'] < 0){
+                $totalNegativo++;
+            }
     }
     ?> 
     <br>
